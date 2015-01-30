7fead59b98674c207e6da0151b60070386d95520
Able Varghese <able.varghese12@gmail.com>
2015-01-30 13:59:47 -0500

    Created generateAbilities method for random number generation and assignment

--- Hero.cs
+++ Hero.cs
@@ -3,33 +3,43 @@
 public class Hero
 {
 
-// Class Properties------------------------------------------------------
- 
- //Public class properties
+    // Class Properties------------------------------------------------------
+
+    //Public class properties
     public string name;
 
- //Private class properties
+    //Private class properties
     private int strength;
     private int speed;
     private int health;
 
 
-// Methods--------------------------------------------------------
-   
-  //Public class methods
+    // Methods--------------------------------------------------------
+
+    //Public class methods
     public void fight();
     public void Show();
 
-  //Private class methods
+    //Private class methods
     private void generateAbilities();
     private bool hitAttempt();
     private int hitDamage();
-    
-public void Hero(string name)
-{ 
-    string name;
-    this.name = name;
-    generateAbilities();
-}
 
+    public void Hero(string name)
+    {
+        string name;
+        this.name = name;
+        generateAbilities();
+    }
+
+    private void generateAbilities()
+    {
+        Random rndNum = new Random();
+        int strength = rndNum.Next(1, 100);
+        int speed = rndNum.Next(1, 100);
+        int health = rndNum.Next(1, 100);
+
+    }
+
+    
 }
